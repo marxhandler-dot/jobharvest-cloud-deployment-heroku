f7283f4c662385709209516d2ced3423d879aa31
SAM <marxhandler@gmail.com>
2026-06-12 01:00:54 +0800

    fifth commit

--- config.py
+++ config.py
@@ -1,3 +1,5 @@
-PAGES_TO_SCRAPE = 2
+import os
+
+PAGES_TO_SCRAPE = int(os.environ.get('PAGES_TO_SCRAPE', 2))
 JOB_SOURCE = "https://www.python.org/jobs"
-LIMIT = 10
+LIMIT = int(os.environ.get('LIMIT', 10))
